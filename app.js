e5a1425694276b6e89b5c844cb7acb02cd704a49
Matias Enkovaara <matias.enkovaara@gmail.com>
2021-07-23 17:27:36 +0300

    mechanic that supports having two colors you need to clear

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () =>
     var squareRects = [];
     var labels = [];
     var checkedSquareAmt = 10;
-    var currentLevelIndex = 0;
+    var currentLevelIndex = 10;
 
     //create a playing board
     function createBoard()
@@ -40,18 +40,25 @@ document.addEventListener('DOMContentLoaded', () =>
         inputs = document.getElementsByTagName('input');
         for (let i=0; i < width*width; i++)
         {
+            
+
             inputs[i].checked = boardArray[i];
-            if(trapInSquareIndex(i))
-            {
-                labels[i].style.backgroundColor = "indianred"
-            }
-            else if(boardArray[i] == true) 
+            if(boardArray[i] == true) 
             {
                 labels[i].style.backgroundColor = "lightgreen";
             }
-            else
+            else if(boardArray[i] == false)
             {
                 labels[i].style.backgroundColor = "whitesmoke";
+                //these ifs here so green squares can overwrite them when pressing undo, because this function is called from undoMove as well. there could be a better solution.
+                if(trapInSquareIndex(i))
+                {
+                    labels[i].style.backgroundColor = "indianred"
+                }
+                else if(fillableInSquareIndex(i))
+                {
+                    labels[i].style.backgroundColor = "lightskyblue"
+                }
             }
         }
     }
@@ -60,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         for(let i=0; i < width*width; i++)
         {
-            labels[i].style.backgroundColor = 'gold';
+            labels[i].style.backgroundColor = 'lightskyblue';
         }
     }
 
@@ -79,13 +86,42 @@ document.addEventListener('DOMContentLoaded', () =>
             else
             {
                 inputs[i].checked = false;
-                labels[i].style.backgroundColor = "whitesmoke";
+                if(fillableInSquareIndex(i)) labels[i].style.backgroundColor = "lightskyblue";
+                else labels[i].style.backgroundColor = "whitesmoke";
                 originalInputValues[i] = false;
                 newSquares.push(inputs[i].checked);
             }
         }
         levelTrapPositions[currentLevelIndex] = []
         levels[currentLevelIndex] = newSquares;
+    }
+
+    function fillableAreaFilledCorrectly()
+    {
+        for(let i=0; i < width*width; i++)
+        {
+            if(fillableInSquareIndex(i) && inputs[i].checked == false)
+            {
+                return false
+            }
+            else if(!fillableInSquareIndex(i) && inputs[i].checked == true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function fillableInSquareIndex(squareIndex)
+    {
+        for(let i=0; i < levelFillablePositions[currentLevelIndex].length; i++)
+        {
+            if(levelFillablePositions[currentLevelIndex][i] == squareIndex)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     function trapInSquareIndex(squareIndex)
@@ -128,6 +164,7 @@ document.addEventListener('DOMContentLoaded', () =>
             labels[i].style.backgroundColor = "whitesmoke";
         }
         currentTrapsArray = [];
+        currentFillablesArray = [];
         levelTrapPositions[currentLevelIndex] = []
     }
 
@@ -156,7 +193,16 @@ document.addEventListener('DOMContentLoaded', () =>
     function createHelpText()
     {
         document.getElementById("help-text").innerHTML =
-        "Your goal is to remove all green squares by clicking and dragging your mouse over them. <br> There are two rules that must be met with each selection you make: <br> 1. You must remove at least one green square. <br> 2. You must create at least one new square. <br> The amount of moves you have per level is limited and is located under the board. Press the 'Done' button when there are no green squares left."
+        "Your goal is to remove all green squares by clicking and dragging your mouse over them. <br> There are two rules and one exception that must be met with each selection you make: <br><br> 1. You must remove at least one green square. <br> 2. You must create at least one new square. <br> Exception: On the last move before solving the level, the move on which you remove all of the remaining green squares, you do not have to create new squares. <br><br> The amount of moves you have per level is limited and is located under the board. Press the 'Done' button when there are no green squares left."
+    }
+
+    const colors = 
+    {
+        GREY: "whitesmoke",
+        GREEN: "lightgreen",
+        BLUE: "lightskyblue",
+        RED: "lightcoral",
+        NONE: "none"
     }
 
     var maximumMoveAmt = 0;
@@ -165,11 +211,18 @@ document.addEventListener('DOMContentLoaded', () =>
     var selectionBox;
     var newCheckboxAmt;
     var removedCheckboxAmt;
+    var blueAmt;
+    var greenAmt;
     var selectionBoxEnabled = true;
+    var lastSelectionColor = colors.GREEN;
+    var originalSelectionColor = colors.NONE;
+    var currentSelectionColor = colors.GREY;
+    var wrongColorAmt = 0;
     var inputs;
     var labels;
     var originalInputValues = [];
-    var currentlySelectedSquares = []
+    var originalSquareColors = [];
+    var currentlySelectedSquares = [];
     var x;
     var y;
     var finX;
@@ -177,7 +230,9 @@ document.addEventListener('DOMContentLoaded', () =>
     var ismousedown = false;
     var editMode = false;
     var editTrapsMode = false;
-    var currentTrapsArray = []
+    var editWallsMode = false;
+    var currentTrapsArray = [];
+    var currentFillablesArray = [];
     
     document.addEventListener('mousedown', mouseDown);
     document.addEventListener('mousemove', mouseMove);
@@ -202,11 +257,14 @@ document.addEventListener('DOMContentLoaded', () =>
     undoButton.addEventListener('mouseover', disableSelectionBox);
     nextLevelButton.addEventListener('mouseover', disableSelectionBox);
     lastLevelButton.addEventListener('mouseover', disableSelectionBox);
-
     function keyDown(event)
     {
         if(event.key == "e") editMode = !editMode;
         if(event.key == "t") editTrapsMode = !editTrapsMode;
+        if(event.key == "w") editWallsMode = !editWallsMode;
+        if(event.key == "1") originalSelectionColor = colors.GREEN;
+        if(event.key == "2") originalSelectionColor = colors.BLUE;
+        if(event.key == "3") originalSelectionColor = colors.GREY;
         if(event.key == "c") clearBoard();
         //reset current level:
         if(event.key == "r") resetLevel();
@@ -225,8 +283,12 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         if(selectionBoxEnabled && currentMoveAmt != 0) //this makes buttons easier to press
         {
+            saveCurrentBoardState();
+            wrongColorAmt = 0;
+            if(!editMode) originalSelectionColor = colors.NONE;
             inputs = document.getElementsByTagName('input');
             setOriginalInputValues();
+            setOriginalSquareColors();
             setAlreadySelectedArray();
             createSelectionBox(event);
             newCheckboxAmt = 0;
@@ -248,25 +310,31 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         if(ismousedown)
         {
-            ismousedown = false;
-            if(selectionDoesNotSatisfyRules() && !editMode) //it's possible that this doesn't solve the problem correctly
+            if(!editMode)
             {
-                setBoardToBeforeSelect();
-            }
-            else
-            {
-                savePreviousBoardState();
-                currentMoveAmt--;
-                updateMovesElement();
-                if(!boxesStillChecked()) 
+                if(selectionDoesNotSatisfyRules()) //it's possible that this doesn't solve the problem correctly
                 {
-                    document.getElementById("move-amount").innerHTML = "Level solved! Press done to move to the next level."
-                    document.getElementById("move-amount").style.color = "green"
+                    setBoardToBeforeSelect();
+                }
+                else
+                {
+                    currentMoveAmt--;
+                    updateMovesElement();
+                    if(!colorStillExists(colors.GREEN) && !colorStillExists(colors.BLUE)) 
+                    {
+                        document.getElementById("move-amount").innerHTML = "Level solved! Press done to move to the next level."
+                        document.getElementById("move-amount").style.color = "green"
+                    }
                 }
             }
+            if(originalSelectionColor != colors.GREY || originalSelectionColor != colors.NONE)
+            {
+                lastSelectionColor = originalSelectionColor;
+            }
+            originalSelectionColor = colors.NONE;
+            currentlySelectedSquares = [];
+            ismousedown = false;
             selectionBox.remove();
-            //logBoardState();
-            //logTraps();
         }
     }
 
@@ -289,10 +357,9 @@ document.addEventListener('DOMContentLoaded', () =>
         }
     }
 
-
     function selectionDoesNotSatisfyRules()
     {
-        return (newCheckboxAmt < 1 || removedCheckboxAmt < 1 || trapInSelectionArea()) && boxesStillChecked();
+        return (newCheckboxAmt < 1 || removedCheckboxAmt < 1 || wrongColorAmt > 0) && colorStillExists(originalSelectionColor);
     }
 
     function disableSelectionBox()
@@ -303,6 +370,16 @@ document.addEventListener('DOMContentLoaded', () =>
     function enableSelectionBox()
     {
         selectionBoxEnabled = true;
+    }
+
+    function colorStillExists(color)
+    {
+        inputs = document.getElementsByTagName('input');
+        for(let i = 0; i < width*width; i++)
+        {
+            if(labels[i].style.backgroundColor == color) return true;
+        }
+        return false;
     }
 
     function boxesStillChecked()
@@ -329,18 +406,19 @@ document.addEventListener('DOMContentLoaded', () =>
         }
     }
 
-    function savePreviousBoardState()
+    function saveCurrentBoardState()
     {
-        previousBoardState = []
+        currentBoardState = []
         for(let i = 0; i < width*width; i++)
         {
-            previousBoardState.push(originalInputValues[i]);
+            currentBoardState.push(labels[i].style.backgroundColor);
         }
-        pastBoardStates.push(previousBoardState);
+        pastBoardStates.push(currentBoardState);
     }
 
     function moveToNextLevel()
     {
+        lastSelectionColor = colors.GREEN;
         currentLevelIndex++;
         document.getElementById("level").innerHTML = "Level " + (currentLevelIndex+1);
         setBoardState(levels[currentLevelIndex]);
@@ -362,7 +440,12 @@ document.addEventListener('DOMContentLoaded', () =>
         {
             currentMoveAmt++;
             updateMovesElement();
-            setBoardState(pastBoardStates.pop());
+            var lastBoardColors = pastBoardStates.pop();
+            console.log(lastBoardColors);
+            for(let i = 0; i < width*width; i++)
+            {
+                labels[i].style.backgroundColor = lastBoardColors[i];
+            }
         }
     }
 
@@ -370,20 +453,7 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         for(let i = 0; i < width*width; i++)
         {
-            inputs[i].checked = originalInputValues[i];
-            if(originalInputValues[i] == true)
-            {
-                labels[i].style.backgroundColor = "lightgreen";
-            }
-            else
-            {
-                labels[i].style.backgroundColor = "whitesmoke";
-            }
-
-            if(trapInSquareIndex(i))
-            {
-                labels[i].style.backgroundColor = "indianred"
-            }
+            labels[i].style.backgroundColor = originalSquareColors[i];
         }
     }
 
@@ -407,6 +477,13 @@ document.addEventListener('DOMContentLoaded', () =>
         console.log(jsonArray);
     }
 
+    function logFillables()
+    {
+       currentFillablesArray.sort();
+       var jsonArray = JSON.stringify(currentFillablesArray);
+       console.log(jsonArray);
+    }
+
     function logTraps()
     {
         currentTrapsArray.sort();
@@ -414,7 +491,107 @@ document.addEventListener('DOMContentLoaded', () =>
         console.log(jsonArray);
     }
 
+    function checkSelectionRectCollision(i)
+    {
+        selectionRect = selectionBox.getBoundingClientRect(); //inefficient for this call to be here
+        return selectionRect.right > inputs[i].getBoundingClientRect().left
+            && selectionRect.bottom > inputs[i].getBoundingClientRect().top
+            && selectionRect.left < inputs[i].getBoundingClientRect().right
+            && selectionRect.top < inputs[i].getBoundingClientRect().bottom
+    }
+
+    function changeSquareColor(i)
+    {
+        currentSelectionColor = labels[i].style.backgroundColor;
+        if(originalSelectionColor == colors.NONE)
+        {
+            originalSelectionColor = currentSelectionColor;
+        }
+
+        //this exists so color of selection starting from grey changes based on what you selected last
+        if(originalSelectionColor == colors.GREY)
+        {
+            if(lastSelectionColor == colors.GREEN)
+            {
+                originalSelectionColor = colors.GREEN;
+            }
+            else if(lastSelectionColor == colors.BLUE)
+            {
+                originalSelectionColor = colors.BLUE;
+            }
+        }
+
+        if(originalSelectionColor == colors.GREEN)
+        {
+            if(currentSelectionColor == colors.GREEN)
+            {
+                //turn current square grey
+                labels[i].style.backgroundColor = colors.GREY;
+                greenAmt--;
+                removedCheckboxAmt++;
+            }
+            else if(currentSelectionColor == colors.GREY)
+            {
+                //turn current square green
+                labels[i].style.backgroundColor = colors.GREEN;
+                greenAmt++;
+                newCheckboxAmt++;
+            }
+            else if(currentSelectionColor == colors.BLUE)
+            {
+                //touched blue, selection fails
+                labels[i].style.backgroundColor = colors.RED;
+                wrongColorAmt++;
+            }
+        }
+        else if(originalSelectionColor == colors.BLUE)
+        {
+            if(currentSelectionColor == colors.BLUE)
+            {
+                //turn current square grey
+                labels[i].style.backgroundColor = colors.GREY;
+                blueAmt--;
+                removedCheckboxAmt++;
+            }
+            else if(currentSelectionColor == colors.GREY)
+            {
+                //turn current square blue
+                labels[i].style.backgroundColor = colors.BLUE;
+                blueAmt++;
+                newCheckboxAmt++;
+            }
+            else if(currentSelectionColor == colors.GREEN)
+            {
+                //touched green, selection fails
+                labels[i].style.backgroundColor = colors.RED;
+                wrongColorAmt++;
+            }
+        }
+    }
+
     function checkIfOverSquare()
+    {
+        for(let i = 0; i < width*width; i++)
+        {
+            var rectCollision = checkSelectionRectCollision(i);
+            if(rectCollision && !currentlySelectedSquares[i])
+            {
+                changeSquareColor(i);
+                currentlySelectedSquares[i] = true;
+            }
+            else if(!rectCollision && currentlySelectedSquares[i])
+            {
+                if(labels[i].style.backgroundColor == colors.RED)
+                {
+                    wrongColorAmt--;
+                }
+                labels[i].style.backgroundColor = originalSquareColors[i];
+                currentlySelectedSquares[i] = false;
+            }
+        }
+    }
+
+    function checkIfOverSquare2() //change name of this function
     {
         selectionRect = selectionBox.getBoundingClientRect();
         for(let i = 0; i < width*width; i++)
@@ -428,10 +605,34 @@ document.addEventListener('DOMContentLoaded', () =>
                 {
                     currentlySelectedSquares[i] = true;
                 }
+                else if(fillableInSquareIndex(i))
+                {
+                    labels[i].style.backgroundColor = "lightskyblue";
+                    currentFillablesArray.push(i);
+                    newCheckboxAmt++;
+                }
                 else if(!currentlySelectedSquares[i])
                 {
                     currentlySelectedSquares[i] = true;
-                    if(originalInputValues[i] == false)
+                    if(editTrapsMode)
+                    {
+                        labels[i].style.backgroundColor = "indianred";
+                        currentTrapsArray.push(i);
+                    }
+                    else if(editWallsMode && !fillableInSquareIndex(i))
+                    {
+                        labels[i].style.backgroundColor = "lightskyblue";
+                        inputs[i].checked = false;
+                        currentFillablesArray.push(i);
+                    }
+                    else if(editWallsMode && fillableInSquareIndex(i))
+                    {
+                        labels[i].style.backgroundColor = "whitesmoke";
+                        inputs[i].checked = false;
+                        index = currentFillablesArray.indexOf(i);
+                        currentFillablesArray.splice(index, 1);
+                    }
+                    else if(originalInputValues[i] == false)
                     {
                         inputs[i].checked = true;
                         labels[i].style.backgroundColor = "lightgreen";
@@ -440,14 +641,9 @@ document.addEventListener('DOMContentLoaded', () =>
                     else
                     {
                         inputs[i].checked = false;
-                        labels[i].style.backgroundColor = "whitesmoke";
+                        if(fillableInSquareIndex(i)) labels[i].style.backgroundColor = "lightskyblue";
+                        else labels[i].style.backgroundColor = "whitesmoke";
                         removedCheckboxAmt++;
-                    }
-
-                    if(editTrapsMode)
-                    {
-                        labels[i].style.backgroundColor = "indianred";
-                        currentTrapsArray.push(i);
                     }
                 }
             }
@@ -457,6 +653,13 @@ document.addEventListener('DOMContentLoaded', () =>
                 if(trapInSquareIndex(i)) //traps cannot be interacted with
                 {
                     currentlySelectedSquares[i] = false;
+                }
+                else if(fillableInSquareIndex(i))
+                {
+                    inputs[i].checked = false;
+                    labels[i].style.backgroundColor = "lightskyblue";
+                    currentFillablesArray.push(i);
+                    newCheckboxAmt--;
                 }
                 else if(originalInputValues[i] == false)
                 {
@@ -471,6 +674,14 @@ document.addEventListener('DOMContentLoaded', () =>
                     removedCheckboxAmt--
                 }
             }
+        }
+    }
+
+    function setOriginalSquareColors()
+    {
+        for(let i = 0; i < width*width; i++)
+        {
+            originalSquareColors[i] = labels[i].style.backgroundColor;
         }
     }
 
